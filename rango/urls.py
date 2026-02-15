6f605355a94e8f17236d03a1695b56667ee5ff8c
Hamadalfarhan <hammadalfarhan@gmail.com>
2026-02-15 00:25:48 +0000

    Completed user registration with UserProfile and image upload support

--- rango/urls.py
+++ rango/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('add_category/', views.add_category, name='add_category'),
     path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
     path('about/', views.about, name='about'),
+    path('register/', views.register, name='register'),
+
 ]
